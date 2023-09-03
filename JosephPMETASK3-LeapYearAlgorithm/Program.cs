3208c6c4fb0708c367934a6b1704a56eda9997d7
BIGJOE8080 <joeoluwafemi80@gmail.com>
2023-09-03 10:57:28 +0100

    'Worked'

---
+++ JosephPMETASK3-LeapYearAlgorithm/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace JosephPMETASK3_LeapYearAlgorithm
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            static bool IsLeapYear(int year)
+            {
+                return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+            }
+
+            // Example usage:
+            Console.WriteLine("Input the Year");
+            var year = int.Parse(Console.ReadLine());
+
+            if (IsLeapYear(year))
+            {
+                Console.WriteLine($"{year} is a leap year.");
+            }
+            else
+            {
+                Console.WriteLine($"{year} is not a leap year.");
+            }
+        }
+    }
+}
